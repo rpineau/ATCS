bfe8ef94e760177e03e2a3f331fd7866cf01bcef
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:44:40 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,17 +22,23 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./mountlist ATCS.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./ATCS.ui" "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/"
-cp "./Astrometric.png" "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/"
-cp "./libATCS.so" "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/"
+cp "./ATCS.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/"
+cp "./Astrometric.png" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/"
+cp "./libATCS.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/mountlist ATCS.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/ATCS.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/Astrometric.png"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/libATCS.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/ATCS.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/Astrometric.png"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/libATCS.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/MountPlugIns/libATCS.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/MountPlugIns/libATCS.so"
 
